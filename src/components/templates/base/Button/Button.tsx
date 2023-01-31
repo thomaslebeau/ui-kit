bdde86b71c4a13719e4e0d82ce742c72e182e864
Thomas Lebeau <dev.thomaslebeau@gmail.com>
2023-01-31 11:43:41 +0100

    feat: init button component

--- src/components/templates/base/Button/Button.tsx
+++ src/components/templates/base/Button/Button.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import './button.css';
+import './Button.css';
 
 interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
    */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
   /**
    * How large should the button be?
    */
@@ -30,16 +26,14 @@ interface ButtonProps {
 export const Button = ({
   primary = false,
   size = 'medium',
-  backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary ? 'button--primary' : 'button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+      className={['button', `button--${size}`, mode].join(' ')}
       {...props}
     >
       {label}
--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button } from './Button';
+import { Button } from '../components/templates/base/Button/Button';
 import './header.css';
 
 type User = {
